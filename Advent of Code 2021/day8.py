d3d99481d06f7c4be1a0f54d9a4cdbee1da7f612
Sxyntheon <89266693+Sxyntheon@users.noreply.github.com>
2021-12-08 11:43:09 +0100

    edited files

--- Advent of Code 2021/day8.py
+++ Advent of Code 2021/day8.py
@@ -1,3 +1,18 @@
 input = open("day8.txt", "r")
 
+INPUT = input.split(" ")
+
+count = 0
+
 for i in input:
+    if len(i) == 2:
+        count += 1
+    elif len(i) == 4:
+        count += 1
+    elif len(i) == 3:
+        count += 1
+    elif len(i) == 7:
+        count += 1
+print(count)
+
+        
